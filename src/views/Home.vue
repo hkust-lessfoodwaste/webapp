33ec8dc66ccb6ab8e969f06fbb252384e5da2ff6
Bruceshark <yyubv@connect.ust.hk>
2023-02-22 16:56:03 +0800

    merge three calls to one api call in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,136 +1,137 @@
 <template>
   <div class="page">
     <main-header />
-    <div class="flex justify-between mx-6 mt-4 mb-2">
-      <div class="text-lg font-bold">Overview</div>
-    </div>
-    <div class="section flex flex-col mx-auto">
-      <div class="flex justify-between">
-        <div class="ring-chart" style="width: 30%">
-          <ring-chart :summary="overviewAvgData" />
-        </div>
-        <div style="width: 70%" class="flex flex-col justify-around">
-          <div class="flex justify-between">
-            <div>
-              <div class="flex">
-                <tableware-icon class="summary-icon" />
-                <span>Overall</span>
+    <a-spin :spinning="isLoading" tip="loading">
+      <div class="flex justify-between mx-6 mt-4 mb-2">
+        <div class="text-lg font-bold">Overview</div>
+      </div>
+      <div class="section flex flex-col mx-auto">
+        <div class="flex justify-between">
+          <div class="ring-chart" style="width: 30%">
+            <ring-chart :summary="overviewAvgData" />
+          </div>
+          <div style="width: 70%" class="flex flex-col justify-around">
+            <div class="flex justify-between">
+              <div>
+                <div class="flex">
+                  <tableware-icon class="summary-icon" />
+                  <span>Overall</span>
+                </div>
+                <div class="text-center">
+                  <span class="font-bold text-xl" v-if="overviewAvgData">{{
+                    overviewAvgData.overall
+                  }}</span
+                  ><span class="font-bold text-xl" v-else>--</span>
+                  <span>%</span>
+                </div>
               </div>
-              <div class="text-center">
-                <span class="font-bold text-xl" v-if="overviewAvgData">{{
-                  overviewAvgData.overall
-                }}</span
-                ><span class="font-bold text-xl" v-else>--</span>
-                <span>%</span>
+              <div>
+                <div class="flex">
+                  <rice-icon class="summary-icon" fill="#F39221" />
+                  <span>Rice</span>
+                </div>
+                <div class="text-center">
+                  <span class="font-bold text-xl" v-if="overviewAvgData">{{
+                    overviewAvgData.rice
+                  }}</span
+                  ><span class="font-bold text-xl" v-else>--</span>
+                  <span>%</span>
+                </div>
+              </div>
+              <div>
+                <div class="flex">
+                  <vege-icon class="summary-icon" fill="#68A885" /> Vege
+                </div>
+                <div class="text-center">
+                  <span class="font-bold text-xl" v-if="overviewAvgData">{{
+                    overviewAvgData.vegetable
+                  }}</span
+                  ><span class="font-bold text-xl" v-else>--</span>
+                  <span>%</span>
+                </div>
+              </div>
+              <div>
+                <div class="flex">
+                  <meat-icon class="summary-icon" fill="#A81E2D" /> Meat
+                </div>
+                <div class="text-center">
+                  <span class="font-bold text-xl" v-if="overviewAvgData">{{
+                    overviewAvgData.meat
+                  }}</span
+                  ><span class="font-bold text-xl" v-else>--</span>
+                  <span>%</span>
+                </div>
               </div>
             </div>
-            <div>
-              <div class="flex">
-                <rice-icon class="summary-icon" fill="#F39221" />
-                <span>Rice</span>
-              </div>
-              <div class="text-center">
-                <span class="font-bold text-xl" v-if="overviewAvgData">{{
-                  overviewAvgData.rice
-                }}</span
-                ><span class="font-bold text-xl" v-else>--</span>
-                <span>%</span>
-              </div>
-            </div>
-            <div>
-              <div class="flex">
-                <vege-icon class="summary-icon" fill="#68A885" /> Vege
-              </div>
-              <div class="text-center">
-                <span class="font-bold text-xl" v-if="overviewAvgData">{{
-                  overviewAvgData.vegetable
-                }}</span
-                ><span class="font-bold text-xl" v-else>--</span>
-                <span>%</span>
-              </div>
-            </div>
-            <div>
-              <div class="flex">
-                <meat-icon class="summary-icon" fill="#A81E2D" /> Meat
-              </div>
-              <div class="text-center">
-                <span class="font-bold text-xl" v-if="overviewAvgData">{{
-                  overviewAvgData.meat
-                }}</span
-                ><span class="font-bold text-xl" v-else>--</span>
-                <span>%</span>
-              </div>
+            <div class="desc text-xs" v-if="overviewAvgData">
+              Average data from
+              {{ timestampToShortDateStr(overviewTime.earliest) }} to
+              {{ timestampToShortDateStr(overviewTime.latest) }}
             </div>
           </div>
-          <div class="desc text-xs" v-if="overviewAvgData">
-            Average data from
-            {{ timestampToShortDateStr(overviewTime.earliest) }} to
-            {{ timestampToShortDateStr(overviewTime.latest) }}
+        </div>
+        <div class="text-xs text-center compare" v-if="overviewRelativeData">
+          You're saving {{ Math.abs(overviewRelativeData.overall) }}%
+          {{ overviewRelativeData.overall < 0 ? "below" : "above" }} the average
+          of HKUST {{ overviewRelativeData.overall < 0 ? "🥺" : "🥳" }}
+        </div>
+      </div>
+
+      <div>
+        <div class="flex justify-between mx-6 mt-4 mb-2">
+          <div class="text-lg font-bold">My badge wall</div>
+          <div class="color-primary self-center" @click="handleToBadgePage">
+            show more
+          </div>
+        </div>
+        <div class="section mx-auto">
+          <div class="flex">
+            <img
+              class="badge-image mx-2"
+              v-for="item in badgeList"
+              :key="item.id"
+              :src="
+                badgePrefix +
+                item.id +
+                (item.progress === 100 ? '_active.png' : '_inactive.png')
+              "
+            />
           </div>
         </div>
       </div>
-      <div class="text-xs text-center compare" v-if="overviewRelativeData">
-        You're saving {{ Math.abs(overviewRelativeData.overall) }}%
-        {{ overviewRelativeData.overall < 0 ? "below" : "above" }} the average
-        of HKUST {{ overviewRelativeData.overall < 0 ? "🥺" : "🥳" }}
-      </div>
-    </div>
-
-    <div class="flex justify-between mx-6 mt-4 mb-2">
-      <div class="text-lg font-bold">My badge wall</div>
-      <div class="color-primary self-center" @click="handleToBadgePage">
-        show more
-      </div>
-    </div>
-
-    <a-spin :spinning="badgeList === null" tip="loading">
-      <div class="section mx-auto">
-        <div class="flex">
-          <img
-            class="badge-image mx-2"
-            v-for="item in badgeList"
-            :key="item.id"
-            :src="
-              badgePrefix +
-              item.id +
-              (item.progress === 100 ? '_active.png' : '_inactive.png')
-            "
-          />
+      <div>
+        <div class="flex justify-between mx-6 mt-4">
+          <div class="text-lg font-bold">My history</div>
+          <div
+            class="color-primary self-center"
+            @click="handleToHistoryPage"
+            v-if="mealList && mealList.length > mealListLenLimit"
+          >
+            show more
+          </div>
         </div>
+        <div v-if="mealList && mealList.length">
+          <div
+            class="flex justify-between items-center"
+            :key="i"
+            v-for="(item, i) in mealList.slice(0, mealListLenLimit)"
+          >
+            <daily-item :data="item" />
+          </div>
+        </div>
+        <div v-else class="m-4 text-center">
+          Share your action and earn badges now 💪
+        </div>
+      </div>
+      <div class="fixed bottom-5 right-3 flex-col image-wrapper pos">
+        <camera-icon class="camera-image" @click="handleToCreatePage" />
       </div>
     </a-spin>
-    <a-spin :spinning="mealList === null" tip="loading">
-      <div class="flex justify-between mx-6 mt-4">
-        <div class="text-lg font-bold">My history</div>
-        <div
-          class="color-primary self-center"
-          @click="handleToHistoryPage"
-          v-if="mealList && mealList.length > mealListLenLimit"
-        >
-          show more
-        </div>
-      </div>
-      <div v-if="mealList && mealList.length">
-        <div
-          class="flex justify-between items-center"
-          :key="i"
-          v-for="(item, i) in mealList.slice(0, mealListLenLimit)"
-        >
-          <daily-item :data="item" />
-        </div>
-      </div>
-      <div v-else class="m-4 text-center">
-        Share your action and earn badges now 💪
-      </div>
-    </a-spin>
-    <div class="fixed bottom-5 right-3 flex-col image-wrapper pos">
-      <camera-icon class="camera-image" @click="handleToCreatePage" />
-    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted,onUpdated, watch } from "vue";
+import { ref, onMounted, onUpdated, watch } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import dailyItem from "@/components/dailyItem.vue";
 import mainHeader from "@/components/mainHeader.vue";
@@ -142,6 +143,7 @@ import meatIcon from "@/assets/meat_icon.svg";
 import vegeIcon from "@/assets/vege_icon.svg";
 import tablewareIcon from "@/assets/tableware_icon.svg";
 import { timestampToShortDateStr } from "@/utils/utils";
+const isLoading = ref(false);
 const overviewAvgData = ref(null);
 const overviewRelativeData = ref(null);
 const overviewTime = ref(null);
@@ -153,24 +155,21 @@ const mealList = ref(null);
 const mealListLenLimit = 3;
 
 const fetchData = async () => {
-  let overviewData = await axiosRequest("get", "overview", {}, true);
-  if (overviewData.error === false) {
-    overviewAvgData.value = overviewData.result.curr_average;
-    overviewRelativeData.value = overviewData.result.relative_average;
-    overviewTime.value = overviewData.result.time;
-  }
-  let mealData = await axiosRequest("get", "history", {}, true);
-  if (mealData.error === false) {
-    mealList.value = mealData.result;
-  }
-  let badgeData = await axiosRequest("get", "badge", {}, true);
-  if (badgeData.error === false) {
-    badgeList.value = badgeData.result;
+  isLoading.value = true;
+  let summaryRes = await axiosRequest("get", "summary", {}, true);
+  isLoading.value = false;
+  if (summaryRes.error === false) {
+    let summaryData = summaryRes.result;
+    overviewAvgData.value = summaryData.overview.curr_average;
+    overviewRelativeData.value = summaryData.overview.relative_average;
+    overviewTime.value = summaryData.overview.time;
+    mealList.value = summaryData.history;
+    badgeList.value = summaryData.badge;
   }
 };
 
 const router = useRouter();
-const route = useRoute()
+const route = useRoute();
 onMounted(() => {
   fetchData();
 });
@@ -178,7 +177,7 @@ watch(
   () => route.params.needReload,
   async (needReload) => {
     if (needReload) {
-      fetchData()
+      fetchData();
     }
   }
 );
